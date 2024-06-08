e463e4180547c504254d304aa498e36f16c73ded
Atilio <coronelatiliomaximiliano@gmail.com>
2024-06-08 18:39:57 -0300

    reconfiguracion de base de datos

--- src/controllers/adminControllers.js
+++
@@ -1,7 +0,0 @@
-const db = require('../database/models')
-
-module.exports = {
-    admin: (req, res) => {
-            res.render('admin/admin')
-    }
-}
--- src/controllers/mainControllers.js
+++
@@ -1,7 +0,0 @@
-const db = require('../database/models')
-
-module.exports = {
-  index : (req,res)=>{zz
-    res.render('index')
-  }
-};
